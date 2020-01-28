94962cb6b99bff98e3ddcbc37a421f8c5c7d52a8
kasiadel <53861206+kasiadel@users.noreply.github.com>
2020-01-28 13:22:32 +0100

    Add comments and console.log to listener

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
+//creating server using node's http Module//
+
 const http = require("http"),
-  fs = require("fs"),
-  url = require("url");
+	fs = require("fs"),
+	url = require("url");
 
 http
-  .createServer((request, response) => {
-    var addr = request.url,
-      q = url.parse(addr, true),
-      filePath = "";
+	.createServer((request, response) => {
+		response.writeHead(200, { "Content-Type": "text/html" });
+		var addr = request.url,
+			q = url.parse(addr, true),
+			filePath = "";
 
-    if (q.pathname.includes("documentation")) {
-      filePath = __dirname + "/documentation.html";
-    } else {
-      filePath = "index.html";
-    }
+		//parsing request.url to check if URL contains word "documentation"
+		//if yes returns documentation.html if not index.html
 
-    fs.appendFile(
-      "log.txt",
-      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
-      function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Added to log.");
-        }
-      }
-    );
-  })
-  .listen(8080);
+		if (q.pathname.includes("documentation")) {
+			filePath = __dirname + "/documentation.html";
+		} else {
+			filePath = "index.html";
+		}
+
+		//appending log of the URL to the end of log.txt file
+		fs.appendFile(
+			"log.txt",
+			"URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+			function(err) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Added to log");
+				}
+			}
+		);
+	})
+	.listen(8080);
+console.log("listening on port 8080");
