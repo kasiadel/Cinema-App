47a096548a645e5beaf8fd6727ea00d610695720
kasiadel <53861206+kasiadel@users.noreply.github.com>
2020-01-29 10:13:58 +0100

    removed errors in code

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-//creating server using node's http Module//
-
 const http = require("http"),
 	fs = require("fs"),
 	url = require("url");
 
 http
 	.createServer((request, response) => {
-		// 		response.writeHead(200, { "Content-Type": "text/html" });
 		var addr = request.url,
 			q = url.parse(addr, true),
 			filePath = "";
-
-		// 		//parsing request.url to check if URL contains word "documentation"
-		// 		//if yes returns documentation.html if not index.html
 
 		if (q.pathname.includes("documentation")) {
 			filePath = __dirname + "/documentation.html";
@@ -20,22 +14,26 @@ http
 			filePath = "index.html";
 		}
 
-		// 		//appending log of the URL to the end of log.txt file
-		fs.appendFile(
-			"log.txt",
-			"URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
-			function(err) {
-				if (err) {
-					console.log(err);
-				} else {
-					console.log("Added to log");
-				}
-
-				response.writeHead(200, { "Content-Type": "text/html" });
-				response.write(data);
-				response.end();
+		fs.readFile(filePath, function(err, data) {
+			if (err) {
+				throw err;
 			}
-		);
+
+			fs.appendFile(
+				"log.txt",
+				"URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+				function(err) {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log("Added to log.");
+					}
+				}
+			);
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.write(data);
+			response.end();
+		});
 	})
 	.listen(8080);
 console.log("listening on port 8080");
